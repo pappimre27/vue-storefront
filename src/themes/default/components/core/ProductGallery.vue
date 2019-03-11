658173558d0f8995a12c2d05a386a1c70d67ef88
Patryk Tomczyk <13100280+patzick@users.noreply.github.com>
2019-03-11 16:19:27 +0100

    test gallery carousel change

--- src/themes/default/components/core/ProductGallery.vue
+++ src/themes/default/components/core/ProductGallery.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="media-gallery" :class="{'media-gallery--loaded': carouselLoaded}">
-    <div v-show="OfflineOnly">
-      <img class="offline-image" v-lazy="offline" :src="offline.src" ref="offline" alt="">
-    </div>
-    <div v-show="OnlineOnly" class="relative">
+    <div class="relative w-100">
       <product-gallery-overlay
         v-if="isZoomOpen"
         :current-slide="currentSlide"
@@ -83,6 +80,8 @@ export default {
   text-align: center;
   width: 100%;
   height: 100%;
+  display: flex;
+  align-items: center;
   min-height: calc(90vw * 1.1);
   background-image: url('/assets/placeholder.svg');
   background-repeat: no-repeat;
